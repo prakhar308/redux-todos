3a9e9486dbd6837e8a8f5006f643fe534eec032f
prakhar308 <khandelwalprakhar30@gmail.com>
2020-06-12 10:08:13 +0530

    add entire code from getting-started-redux by Dan

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import AddTodo from '../containers/AddTodo';
+import VisibleTodoList from '../containers/VisibleTodoList';
+import Footer from '../components/Footer';
 
 function App() {
 	return (
 		<div>
-			TodosApp
+			<AddTodo />
+			<VisibleTodoList />
+			<Footer />
 		</div>
 	);
 }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import rootReducer from './reducers/';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
+const store = createStore(rootReducer)
 
 ReactDOM.render(
-   <App />,
+	<Provider store={store}>
+		<App />
+	</Provider>,
   	document.getElementById('root')
 );
